81b1b71de79f61c99d8397d5641b132b81ab6cb6
chen496 <cxw496@miami.edu>
2021-09-16 00:32:13 -0400

    Update AIMNet.c

--- code_simulatedData/AIMNet.c
+++ code_simulatedData/AIMNet.c
@@ -14,19 +14,19 @@ if(argc<=2)
 
     if(num_condit==2){
         simu_2condits(argv);
-        printf("**** AIMNet Finished ****\n\n");
+        printf("**** AIMNet Finished: 2 conditions ****\n\n");
     }
     if(num_condit==3){
         simu_3condits(argv);
-        printf("**** AIMNet Finished ****\n\n");
+        printf("**** AIMNet Finished: 3 conditions ****\n\n");
     }
     if(num_condit==4){
         simu_4condits(argv);
-        printf("**** AIMNet Finished ****\n\n");
+        printf("**** AIMNet Finished: 4 conditions ****\n\n");
     }
     if(num_condit==5){
         simu_5condits(argv);
-        printf("**** AIMNet Finished ****\n\n");
+        printf("**** AIMNet Finished: 5 conditions ****\n\n");
     }
     if(num_condit>5){
         printf("\n The number of conditions/tissues can't be greater than 5.\n");
